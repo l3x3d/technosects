a579a26b4f36d67fa424d019e083bc13db2ffd2d
l3x3d <admin@falsezero.io>
2023-03-31 16:56:52 -0700

    modified:   components/IndexPage.tsx

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -38,7 +38,6 @@ export default function IndexPage(props: IndexPageProps) {
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
-        <IntroTemplate />
       </Layout>
     </>
   )
