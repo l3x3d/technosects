661a7f8ad1c11bc78e2971fae681aee6c867b17f
l3x3d <admin@falsezero.io>
2023-04-01 00:02:46 -0700

    modified:   schemas/marquee.ts 	new file:   schemas/myDocumentType.ts

--- schemas/marquee.ts
+++ schemas/marquee.ts
@@ -1,30 +1,48 @@
+// ./schemas/marquee.ts
 
-export default {
-    name: 'marquee',
-    type: 'object',
-    fields: [
-      {
-        name: 'text',
-        type: 'string',
-        validation: (Rule) => Rule.required(),
+import { SchemaType } from '@sanity/types';
+import { PortableTextFeatures } from '@sanity/portable-text-editor';
+
+const marqueeModule: SchemaType = {
+  name: 'marqueeModule',
+  title: 'Marquee Module',
+  type: 'object',
+  fields: [
+    {
+      name: 'text',
+      title: 'Text',
+      type: 'string',
+      description: 'The text to be scrolled.',
+      validation: (Rule: any) => Rule.required(),
+    },
+    {
+      name: 'speed',
+      title: 'Speed',
+      type: 'number',
+      description: 'The speed of the scroll in milliseconds.',
+      validation: (Rule: any) => Rule.min(1),
+      inputComponent: 'Slider',
+      options: {
+        // Customize the slider range and step size
+        min: 1,
+        max: 1000,
+        step: 50,
       },
-      {
-        name: 'speed',
-        type: 'number',
-        defaultValue: 100,
+    },
+    {
+      name: 'direction',
+      title: 'Direction',
+      type: 'string',
+      description: 'The direction of the scroll.',
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        list: [
+          { title: 'Left', value: 'left' },
+          { title: 'Right', value: 'right' },
+        ],
       },
-      {
-        name: 'direction',
-        type: 'string',
-        options: {
-          list: [
-            { title: 'Left', value: 'left' },
-            { title: 'Right', value: 'right' },
-          ],
-          layout: 'radio',
-        },
-        defaultValue: 'left',
-      },
-    ],
-  };
-  
+    },
+  ],
+};
+
+export default marqueeModule;
