c9b30c693486640f8bc08ee084ffc642df63c584
l3x3d <admin@falsezero.io>
2023-03-31 19:16:16 -0700

    modified:   pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-black text-whites">
+      <body className="bg-black text-white">
         <Main />
         <NextScript />
       </body>
