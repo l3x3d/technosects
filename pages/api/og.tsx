00f1b31ec4806ccd9e86b75ed59420b860f5ae1a
l3x3d <admin@falsezero.io>
2023-04-05 16:59:03 -0700

    modified:   pages/api/og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -7,7 +7,7 @@ import { createClient } from 'next-sanity'
 export const config: PageConfig = { runtime: 'experimental-edge' }
 
 import { height, OpenGraphImage, width } from 'components/OpenGraphImage'
-import * as demo from 'lib/demo.data'
+
 import { Settings, settingsQuery } from 'lib/sanity.queries'
 
 export default async function og(req: NextRequest, res: NextResponse) {
